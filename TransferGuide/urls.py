9623ab6ad945f9fac70b2a5667acc29dc853b1d7
Xuhao Cui <xcui5@ncsu.edu>
2023-03-15 15:53:38 -0400

    new html

--- TransferGuide/urls.py
+++ TransferGuide/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('ClassInfos/', views.Info, name='Info'),
+    path('accounts/logout1/', views.logoutt, name='logout')
 ]
--- TransferGuide/views.py
+++ TransferGuide/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import logout
 
 
 def index(request):
     return render(request, 'TransferGuide/login.html')
 
+
 def Info(request):
     return render(request, 'TransferGuide/ClassInfo.html')
+
+
+def logoutt(request):
+    logout(request)
+    return render(request, 'TransferGuide/login.html')
+
+
