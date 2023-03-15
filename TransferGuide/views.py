f365fc7ffe33b35bc24a9779fad7c326e45e79f7
Xuhao Cui <xcui5@ncsu.edu>
2023-03-14 22:20:07 -0400

    sprint 4

--- TransferGuide/views.py
+++ TransferGuide/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'TransferGuide/login.html')
+
+def Info(request):
+    return render(request, 'TransferGuide/ClassInfo.html')
