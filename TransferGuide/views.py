62e33d09d1b90638a6c5411ac17ff35f55ec74cf
rileyimmel <ypq8st@virginia.edu>
2023-03-20 13:11:16 -0400

    Add courseSearch

--- TransferGuide/views.py
+++ TransferGuide/views.py
@@ -5,13 +5,11 @@ from django.template import loader
 from django.views import generic
 
 from .tasks import sisBackground
-from django.db import models
 from .models import Course
-from django.core.exceptions import ObjectDoesNotExist
-from django.core.exceptions import MultipleObjectsReturned
 from django.shortcuts import redirect
 import requests
 
+from django.db.models import Q
 subjectList = []
 
 
@@ -99,3 +97,117 @@ class CoursesViewAll(generic.ListView):
             "courses": Course.objects.all().order_by('courseSubject', 'courseNumber'),
             "subjects": Course.objects.all().values('courseSubject').order_by('courseSubject').distinct()
         }
+
+
+class SearchResultsView(generic.ListView):
+    model = Course
+    template_name = 'TransferGuide/search_results.html'
+    context_object_name = 'all_courses_list'
+
+    # Need to add search based on university once that is set up
+    def get_queryset(self):
+        subject_query = self.request.GET.get("subject")
+        number_query = self.request.GET.get("number")
+        name_query = self.request.GET.get("name")
+        print(subject_query, number_query, name_query)
+        queryset = {}
+        if subject_query is not None:
+            subject_query = subject_query.upper()
+        if name_query is None:
+            name_query = ''
+
+        # All 3 parameters so should be &
+        if (subject_query != '') & (number_query != '') & (name_query != ''):
+            queryset = {
+                "courses": Course.objects.filter(
+                    Q(courseSubject=subject_query) & Q(courseNumber=number_query) & Q(courseName__icontains=name_query))
+                .order_by('courseSubject', 'courseNumber'),
+                "subjects": Course.objects.filter(
+                    Q(courseSubject=subject_query) & Q(courseNumber=number_query) & Q(courseName__icontains=name_query))
+                .values('courseSubject').order_by('courseSubject').distinct()
+            }
+
+        # Subject and Number
+        elif (subject_query != '') & (number_query != ''):
+            queryset = {
+                "courses": Course.objects.filter(
+                    Q(courseSubject=subject_query) & Q(courseNumber=number_query))
+                .order_by('courseSubject', 'courseNumber'),
+                "subjects": Course.objects.filter(
+                    Q(courseSubject=subject_query) & Q(courseNumber=number_query))
+                .values('courseSubject').order_by('courseSubject').distinct()
+            }
+
+        # Subject and Name
+        elif (subject_query != '') & (name_query != ''):
+            queryset = {
+                "courses": Course.objects.filter(
+                    Q(courseSubject=subject_query) & Q(courseName__icontains=name_query))
+                .order_by('courseSubject', 'courseNumber'),
+                "subjects": Course.objects.filter(
+                    Q(courseSubject=subject_query) & Q(courseName__icontains=name_query))
+                .values('courseSubject').order_by('courseSubject').distinct()
+            }
+
+        # Number and Name
+        elif (number_query != '') & (name_query != ''):
+            queryset = {
+                "courses": Course.objects.filter(
+                    Q(courseNumber=number_query) & Q(courseName__icontains=name_query))
+                .order_by('courseSubject', 'courseNumber'),
+                "subjects": Course.objects.filter(
+                    Q(courseNumber=number_query) & Q(courseName__icontains=name_query))
+                .values('courseSubject').order_by('courseSubject').distinct()
+            }
+        # Just Subject
+        elif subject_query != '':
+            queryset = {
+                "courses": Course.objects.filter(
+                    Q(courseSubject=subject_query))
+                .order_by('courseSubject', 'courseNumber'),
+                "subjects": Course.objects.filter(
+                    Q(courseSubject=subject_query))
+                .values('courseSubject').order_by('courseSubject').distinct()
+            }
+
+        # Just Number
+        elif number_query != '':
+            queryset = {
+                "courses": Course.objects.filter(
+                    Q(courseNumber=number_query))
+                .order_by('courseSubject', 'courseNumber'),
+                "subjects": Course.objects.filter(
+                    Q(courseNumber=number_query))
+                .values('courseSubject').order_by('courseSubject').distinct()
+            }
+
+        # Just Name
+        elif name_query != '':
+            queryset = {
+                "courses": Course.objects.filter(
+                    Q(courseName__icontains=name_query))
+                .order_by('courseSubject', 'courseNumber'),
+                "subjects": Course.objects.filter(
+                    Q(courseName__icontains=name_query))
+                .values('courseSubject').order_by('courseSubject').distinct()
+            }
+
+        if queryset is not None:
+            courses_got = queryset.get('courses')
+            subjects_got = queryset.get('subjects')
+
+            if (courses_got != '') & (courses_got is not None):
+                courses_count = len(queryset.get('courses'))
+            else:
+                courses_count = 0
+            if (subjects_got != '') & (subjects_got is not None):
+                subjects_count = len(queryset.get('subjects'))
+            else:
+                subjects_count = 0
+
+            if (courses_count == 0) & (subjects_count == 0):
+                return {}
+            else:
+                return queryset
+        else:
+            return queryset
