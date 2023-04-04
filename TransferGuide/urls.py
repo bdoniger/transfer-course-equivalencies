361351a61ac73d17fbfbb6dc7127c5346541e8d4
rileyimmel <ypq8st@virginia.edu>
2023-04-03 20:04:55 -0400

    change index to as_view

--- TransferGuide/urls.py
+++ TransferGuide/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index.as_view(), name='index'),
     path('ClassInfos/', views.Info, name='Info'),
     path('accounts/logout/', views.logoutt, name='logout'),
     path('sisUpdate/<semester>/<page>/<subjectNum>/', views.sisUpdate, name='update'),
