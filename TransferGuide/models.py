cbdbe962129a056a7b04b2270c5ef7835d929914
esv3ju <esv3ju@virginia.edu>
2023-04-11 20:53:23 -0400

    Fixed university name issues
    Added another field for full university name and changed university to the university acronym. Additionally updated all current code to work with this new formatting. Lastly I changed all university filters to be not case sensitive.

--- TransferGuide/models.py
+++ TransferGuide/models.py
@@ -7,7 +7,8 @@ class Course(models.Model):
     courseName = models.CharField(max_length=200, default="N/A")  # name of course
     courseNumber = models.CharField(max_length=5, default="N/A")  # number of course
     courseSubject = models.CharField(max_length=20, default="N/A")  # department of course, i.e. CS
-    university = models.CharField(max_length=100, default="UVA")  # UVA or other
+    universityShort = models.CharField(max_length=100, default="UVA")  #acronym, such as UVA
+    universityLong = models.CharField(max_length=100, default="University Of Virginia")  # Full name so university of virginia
     equivalentCourse = models.JSONField(encoder=None, decoder=None, default=dict, null=True, blank=True)
     nonEquivalentCourse = models.JSONField(encoder=None, decoder=None, default=dict, null=True, blank=True)
 
@@ -19,7 +20,8 @@ class requestForm(models.Model):
     courseName = models.CharField(max_length=200, default="N/A")  # name of course
     courseNumber = models.CharField(max_length=5, default="N/A")  # number of course
     courseSubject = models.CharField(max_length=20, default="N/A")  # department of course, i.e. CS
-    university = models.CharField(max_length=100, default="N/A")  # UVA or other
+    university = models.CharField(max_length=100, default="N/A")  # Full name so University of Virginia
+    universityShort = models.CharField(max_length=100, default="N/A") # acronym so UVA
     url = models.CharField(max_length=1000, default="N/A")  # url to class webpage
     studentName = models.ForeignKey(User, on_delete=models.CASCADE)
     studentEmail = models.CharField(max_length=1000, default="N/A")
@@ -33,3 +35,6 @@ class requestForm(models.Model):
 
 class user(models.Model):
     isAdmin = models.BooleanField(default=True)
+
+class acronym(models.Model):
+    nameAcronymTable = models.JSONField(encoder=None, decoder=None, default=dict, null=True, blank=True)
